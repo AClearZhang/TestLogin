4417210e926889973deba75334a8fdb020862ab4
AclearZhang <aclearzhang@qq.com>
2017-09-25 23:50:36 +0800

    success drag

--- drag.js
+++ drag.js
@@ -3,17 +3,16 @@ function drag (id){
 	var obj =  document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
-	obj.onmousedown = function(){
+	obj.onmousedown = function(ev){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
 		document.onmousemove = function(ev){
-			obj.style.left = ev.clienrX  - disX + 'px';
-			obj.style.top = ev.clienrY  - disY + 'px';
-			
-			obj.style.left = ev.clienrX  - disX + 'px';
-			obj.style.top = ev.clienrY  - disY + 'px';
+			obj.style.left = ev.pageX  - disX + 'px';
+			obj.style.top = ev.pageY  - disY + 'px';
 
 		}
+		document.onmousemove = null;
+		document.onmouseup  = null;
 	}
-
+	return false;
 }
